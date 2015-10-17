0ed255df7ad062cab6ae4ccb9ae0abc68fda6933
reillyethan <lekskazimirchuk@gmail.com>
2015-10-17 15:26:51 +0300

    Initial commit. Created ember app and generated auth routes

---
+++ app/router.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('login', {});
+  this.route('register', {});
+  this.route('logout', {});
+});
+
+export default Router;

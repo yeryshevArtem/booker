34435a9d8ea38f92d7f22511819b3f3b1c8bf461
Zippi-wzp <truebatmandc@gmail.com>
2015-10-17 15:38:53 -0400

    Added incomes route and its subroutes

--- app/router.js
+++ app/router.js
@@ -9,6 +9,10 @@ Router.map(function() {
   this.route('login', {});
   this.route('register', {});
   this.route('logout', {});
+  this.route('incomes', {}, function() {
+    this.route('add', {});
+    this.route('edit', {});
+  });
 });
 
 export default Router;

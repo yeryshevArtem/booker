d938e648d3a42369342c8a8b640ea54c2eb8af0b
reillyethan <lekskazimirchuk@gmail.com>
2015-10-19 23:34:34 +0300

    Edited templates a little bit, added application route

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Route.extend({
+  actions: {
+    login: function (email, password) {
+      return alert("To be done.");
+    },
+    loginViaSocialNetwork: function () {
+      return alert("To be done.");
+    }
+  }
 });

2b6930b75e515cd9fb1897408232af3c512c2816
reillyethan <lekskazimirchuk@gmail.com>
2015-10-20 20:07:53 +0300

    Toggled isAuthenticated property

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.Object.create({
-      isAuthenticated: false
+      isAuthenticated: true
     });
   }
 });

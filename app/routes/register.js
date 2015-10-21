c0abe3aaea6062f01bd8c79b56f1d2553f10d00b
Zippi-wzp <truebatmandc@gmail.com>
2015-10-20 20:00:58 -0400

    Edited template and action

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Route.extend({
+  actions: {
+    register: function (nickName, email, password, repeatedPassword) {
+      if (nickName && email && password && repeatedPassword) {
+        if (password == repeatedPassword) {
+          return alert("Password repeated correctly.");
+        } else {
+          return alert("Wrong password!");
+        }
+      }
+      return alert("All fields are blank.");
+    }
+  }
 });

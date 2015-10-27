338a422030f250506d25209835a7e3e17a4e16ea
Zippi-wzp <truebatmandc@gmail.com>
2015-10-27 21:04:44 +0200

    Added tests.

---
+++ tests/integration/components/modal-window-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('modal-window', 'Integration | Component | modal window', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{modal-window}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#modal-window}}
+      template block text
+    {{/modal-window}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});

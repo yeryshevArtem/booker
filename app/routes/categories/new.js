8152413cc1aedb1f8bf9a55d33e4f700dbbc4d9a
Zippi-wzp <truebatmandc@gmail.com>
2015-10-30 19:20:05 +0200

    Closes #3

--- app/routes/categories/new.js
+++ app/routes/categories/new.js
@@ -6,15 +6,9 @@ export default Ember.Route.extend({
   },
   actions: {
     create: function (category) {
-      function getRandomArbitrary(min, max) {
-        var random =  Math.random() * (max - min) + min;
-        var roundRandom = Math.round(random);
-        return roundRandom;
-      };
       // console.log(category);
       // console.log(category.get("name"));
       this.store.createRecord("category", {
-        id:  getRandomArbitrary(1, 100),
         name: category.get("name"),
         icon: category.get("icon")
       }).save().then(function (savedModelInstance) {
--- app/routes/wallets/new.js
+++ app/routes/wallets/new.js
@@ -6,13 +6,7 @@ export default Ember.Route.extend({
   },
   actions: {
     create: function (wallet) {
-      function getRandomArbitrary(min, max) {
-        var random =  Math.random() * (max - min) + min;
-        var roundRandom = Math.round(random);
-        return roundRandom;
-      };
       this.store.createRecord("wallet", {
-        id:  getRandomArbitrary(1, 100),
         name: wallet.get("name"),
         balance: wallet.get("balance")
       }).save().then(function (savedModelInstance) {

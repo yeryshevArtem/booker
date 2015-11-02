93ac483ec10dbf48fe138e049a582d5ff4b24b0b
Zippi-wzp <truebatmandc@gmail.com>
2015-11-02 13:49:20 +0200

    Added package ember-validations and imported object EmberValidation.

--- app/models/category.js
+++ app/models/category.js
@@ -1,6 +1,15 @@
 import DS from 'ember-data';
+// import EmberValidations from "ember-validations";
 
 export default DS.Model.extend({
   name: DS.attr(),
-  icon: DS.attr()
+  icon: DS.attr(),
+  // validations: {
+  //   name: {
+  //     presence: true
+  //   },
+  //   balance: {
+  //     presence: true
+  //   }
+  // }
 });
--- app/models/wallet.js
+++ app/models/wallet.js
@@ -1,6 +1,16 @@
 import DS from 'ember-data';
+// import EmberValidations from "ember-validations";
 
 export default DS.Model.extend({
   name: DS.attr(),
-  balance: DS.attr()
+  balance: DS.attr(),
+  // validations: {
+  //   name: {
+  //     presence: true
+  //   },
+  //   balance: {
+  //     presence: true,
+  //     numericality: true
+  //   }
+  // }
 });

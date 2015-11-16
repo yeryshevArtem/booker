c48f1c543bdeda130d125c737ec10fddd31f1d4d
Zippi-wzp <truebatmandc@gmail.com>
2015-11-17 00:22:17 +0200

    Deleted objects email and login.

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,9 +1,8 @@
 import Ember from 'ember';
-import DS from 'ember-data';
 
 export default Ember.Route.extend({
   actions: {
-    login: function (email, password) {
+    login: function () {
       return alert("To be done.");
     },
     loginViaSocialNetwork: function () {

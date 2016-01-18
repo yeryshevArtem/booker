4281687b3b9599114fe1f381b216c98a9c1d4ec0
kazimirchuk <lekskazimirchuk@gmail.com>
2016-01-18 23:02:36 +0200

    Attempt to iterate through object's attributes.

--- app/components/table-body-row.js
+++ app/components/table-body-row.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   content: null, //pass object instance
-  values: function () {
-    // return [];
-  },
+  contentObserver: Ember.on('init', Ember.observer('content.name', function() {
+    //TODO:
+    //1. Get content.
+    //2. Make it have only attributes/values we need (described in model).
+  })),
   actions: {
     deleteEntity: function () {
       this.sendAction("deleteEntity", this.get('content')); //pass content
